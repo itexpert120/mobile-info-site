7294093db1e82d93aa6ab451b399d0c81ecfb906
itexpert120 <itexpert120@gmail.com>
2023-08-19 18:24:09 +0500

    feat: move heading to brands list div

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import RecommendedDevices from "@/components/recommended-list";
 export default async function Home() {
   return (
     <main className="max-w-sm sm:max-w-6xl mx-auto">
-      <Heading title="Top Brands" />
       <div className="flex flex-col-reverse md:flex-row items-center gap-x-4">
         <div className="md:min-w-[256px] min-w-full">
+          <Heading title="Top Brands" />
           <BrandsList />
         </div>
         <div className="flex-1 md:flex-auto w-full flex items-center justify-center">
